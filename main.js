7d408dc90dc7f9e84c93e1766f4a9e4f0a45c01c
Kacper Grudziński <kgrvdzin@gmail.com>
2023-09-20 11:33:30 +0200

    BEM changes

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const firstName = 'Eryk';
+const firstName = "Eryk";
 const age = 23;
 
 console.log(firstName);
 console.log(age);
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lata.`);
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Siema, nazywam się ${firstName} i mam ${age} lata.`
+
+const box = document.querySelector('.empty-box__js')
+
+box.innerHTML = 'Uważam, że ma on przed sobą świetlaną przyszłość'
